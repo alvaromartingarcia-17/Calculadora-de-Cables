b84eae6fb15f1c1502a8d3874fe413ceb8b9c660
Marta <martax.medina@gmail.com>
2026-04-15 08:41:25 +0200

    Marta 15/04/2026 Diseño inicial interfaz

--- app/src/main/java/com/example/calculadoradecables/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradecables/MainActivity.kt
@@ -10,11 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        //setContentView(R.layout.activity_main)
+        //setContentView(R.layout.pantalla_inicio_sesion)
+        //setContentView(R.layout.pantalla_menu_principal)
+        setContentView(R.layout.pantalla_listado_cables)
+        //setContentView(R.layout.listado_cables)
     }
 }
