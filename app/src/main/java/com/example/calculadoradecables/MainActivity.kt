42b48439ff977f6b3a9e6acdd5ef785ddba057bc
Marta <martax.medina@gmail.com>
2026-04-27 09:24:15 +0200

    Marta 27/04/2026 Mejorado diseño interfaz

--- app/src/main/java/com/example/calculadoradecables/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradecables/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.pantalla_listado_cables)
         //setContentView(R.layout.listado_cables)
         //setContentView(R.layout.pantalla_datos_cable1)
-        setContentView(R.layout.pantalla_datos_cable2)
+        //setContentView(R.layout.pantalla_datos_cable2)
+        //setContentView(R.layout.pantalla_datos_cable3)
+        setContentView(R.layout.pantalla_datos_cable4)
     }
 }
